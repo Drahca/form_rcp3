8d5690659d3b8f1aa15a5dbabc1ddd77ce62ca15
Drahc <Drahc@host.docker.internal>
2020-08-25 17:47:42 +0200

    Augmentation de l'UI d'information de la réservation

--- com.scalian.rental.ui/src/com/scalian/rental/ui/view/ViewInfo.java
+++ com.scalian.rental.ui/src/com/scalian/rental/ui/view/ViewInfo.java
@@ -13,6 +13,9 @@ import com.scalian.rental.core.RentalCoreActivator;
 
 public class ViewInfo extends ViewPart {
 	private Label rentedObjectLabel;
+	private Label lbl_locataire;
+	private Label lbl_dateDeb;
+	private Label lbl_dateFin;
 	
 	public ViewInfo() {
 		// TODO Auto-generated constructor stub
@@ -24,20 +27,48 @@ public class ViewInfo extends ViewPart {
 		parent.setLayout(new GridLayout(1, false));
 		
 		Group infoGroup = new Group(parent, SWT.None);
+		infoGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		infoGroup.setText("Information");
 		infoGroup.setLayout(new GridLayout(2, false));
 		
-		rentedObjectLabel = new Label(infoGroup, SWT.BORDER);
+		rentedObjectLabel = new Label(infoGroup, SWT.NONE);
 		GridData gd = new GridData();
-		gd.horizontalSpan = 2;
+		gd.grabExcessHorizontalSpace = true;
+		gd.horizontalSpan = 3;
 		gd.horizontalAlignment = SWT.FILL;
 		rentedObjectLabel.setLayoutData(gd);
+		
+		Label lblNewLabel = new Label(infoGroup, SWT.NONE);
+		lblNewLabel.setText("Lou\u00E9 \u00E0 :");
+		
+		lbl_locataire = new Label(infoGroup, SWT.NONE);
+		lbl_locataire.setText("New Label");
+		
+		Group grpDatesDeRservations = new Group(parent, SWT.NONE);
+		grpDatesDeRservations.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		grpDatesDeRservations.setText("Dates de r\u00E9servations");
+		grpDatesDeRservations.setLayout(new GridLayout(2, false));
+		
+		Label lblNewLabel_2 = new Label(grpDatesDeRservations, SWT.NONE);
+		lblNewLabel_2.setText("De :");
+		
+		lbl_dateDeb = new Label(grpDatesDeRservations, SWT.NONE);
+		lbl_dateDeb.setText("New Label");
+		
+		Label lblNewLabel_4 = new Label(grpDatesDeRservations, SWT.NONE);
+		lblNewLabel_4.setText("A :");
+		
+		lbl_dateFin = new Label(grpDatesDeRservations, SWT.NONE);
+		lbl_dateFin.setText("New Label");
 		
 		setRental(RentalCoreActivator.getAgency().getRentals().get(0));
 	}
 	
 	public void setRental(Rental r) {
 		rentedObjectLabel.setText(r.getRentedObject().getName());
+		lbl_locataire.setText(r.getCustomer().getDisplayName());
+		lbl_dateDeb.setText(r.getStartDate().toString());
+		lbl_dateFin.setText(r.getEndDate().toString());
 	}
 
 	@Override
