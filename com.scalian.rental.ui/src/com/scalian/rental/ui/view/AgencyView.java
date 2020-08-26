b9831656e19bd6a5bc0fa3d297a094b80c0bed90
Drahc <Drahc@host.docker.internal>
2020-08-26 17:44:23 +0200

    Prise en compte de la modification des couleurs de la préférence

--- com.scalian.rental.ui/src/com/scalian/rental/ui/view/AgencyView.java
+++ com.scalian.rental.ui/src/com/scalian/rental/ui/view/AgencyView.java
@@ -2,24 +2,43 @@ package com.scalian.rental.ui.view;
 
 import java.util.Arrays;
 
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.ViewPart;
 
 import com.scalian.rental.core.RentalCoreActivator;
+import com.scalian.rental.ui.RentalUIActivator;
 
-public class AgencyView extends ViewPart {
+public class AgencyView extends ViewPart implements IPropertyChangeListener {
 	
 	public static final String ID = "com.scalian.rental.ui.view.agencyview";
+	private TreeViewer tv;
 	
 	public AgencyView() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void createPartControl(Composite parent) {
+	public void init(IViewSite site) throws PartInitException {
 		// TODO Auto-generated method stub
-		TreeViewer tv = new TreeViewer(parent);
+		super.init(site);
+		RentalUIActivator.getDefault().getPreferenceStore().addPropertyChangeListener(this);
+	}
+	
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		RentalUIActivator.getDefault().getPreferenceStore().removePropertyChangeListener(this);
+		super.dispose();
+	}
+	
+	@Override
+	public void createPartControl(Composite parent) {
+		tv = new TreeViewer(parent);
 		tv.setContentProvider(new RentalProvider());
 		tv.setLabelProvider(new RentalProvider());
 		tv.setInput(Arrays.asList(RentalCoreActivator.getAgency()));
@@ -30,7 +49,13 @@ public class AgencyView extends ViewPart {
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		// TODO Auto-generated method stub
+		tv.refresh();
 	}
 
 }
