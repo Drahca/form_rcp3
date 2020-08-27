4a0a5732cacca1b54bf4c09aac795eb36cbeebfc
Drahc <Drahc@host.docker.internal>
2020-08-27 14:43:19 +0200

    Commit du commande

--- com.scalian.rental.ui.cmd/src/com/scalian/rental/ui/cmd/handler/CustomerCopy.java
+++ com.scalian.rental.ui.cmd/src/com/scalian/rental/ui/cmd/handler/CustomerCopy.java
@@ -4,13 +4,17 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.ImageTransfer;
 import org.eclipse.swt.dnd.RTFTransfer;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.opcoach.training.rental.Customer;
+import com.scalian.rental.ui.RentalUIActivator;
+import com.scalian.rental.ui.RentalUIConstants;
 
 public class CustomerCopy extends AbstractHandler {
 
@@ -25,11 +29,16 @@ public class CustomerCopy extends AbstractHandler {
 		 	
 			String textData = ((Customer) selected).getDisplayName();
 			String rtfData = "{\\rtf1\\b\\i "+ ((Customer) selected).getDisplayName() +"}";
+			ImageData imgData = RentalUIActivator.getDefault().getImageRegistry().get(RentalUIConstants.IMG_AGENCY).getImageData();
+			
 			
 			TextTransfer textTransfer = TextTransfer.getInstance();
 			RTFTransfer rtfTransfer = RTFTransfer.getInstance();
-			Transfer[] transfers = new Transfer[]{textTransfer, rtfTransfer};
-			Object[] data = new Object[]{textData, rtfData};
+			ImageTransfer imgTransfert = ImageTransfer.getInstance();
+			
+			Transfer[] transfers = new Transfer[]{textTransfer, rtfTransfer, imgTransfert};
+			Object[] data = new Object[]{textData, rtfData, imgData};
+			
 			clipboard.setContents(data, transfers);
 			clipboard.dispose();
 	 	}
